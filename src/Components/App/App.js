004378861e975d8e4651d9f34cff5c37336be453
Jacob Honoré <643471+JacobHonore@users.noreply.github.com>
2018-10-10 22:24:51 +0200

    Implement functions for saving playlist and handling search

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,12 +19,15 @@ class App extends Component {
         id: 0,
         name: '',
         artist: '',
-        album: ''
+        album: '',
+        uri: ''
       }]
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlayListName = this.updatePlayListName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
   addTrack(track) {
     if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
@@ -48,15 +51,24 @@ class App extends Component {
       playlistName: name
     });
   }
+  savePlaylist() {
+    let trackURIs = [];
+    this.state.playlistTracks.forEach(function(track) {
+      trackURIs.push(track.uri);
+    });
+  }
+  search(term) {
+    console.log(term);
+  }
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlayListName} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlayListName} onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
