7464ab098c136412031d839a7420a82fc65237d8
Jacob Honoré <643471+JacobHonore@users.noreply.github.com>
2018-10-15 20:11:04 +0200

    Fix so playlistname is able to update when setting state

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -13,7 +13,7 @@ class Playlist extends Component {
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={this.props.playlistName} onChange={this.handleNameChange} />
+        <input value={this.props.playlistName} onChange={this.handleNameChange} />
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
         <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
       </div>
